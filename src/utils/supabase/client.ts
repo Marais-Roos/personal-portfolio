85fdab9bd6cfd85a7bb049eb535b972b056cba2c
Marais Roos <maraisroos@outlook.com>
2025-11-01 01:25:12 +0200

    Changes to api calls

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -10,8 +10,8 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     // process.env.NEXT_PUBLIC_SUPABASE_URL should be defined in .env.local
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY should be defined in .env.local
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_ANON_KEY!
   )
 }
